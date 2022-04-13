94418fcbb882515117788b89b95a59597f75d597
Shakthi <shakthishamruth@gmail.com>
2022-04-14 01:10:27 +0530

    main

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ pygame.display.set_caption('XO')
 icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 background = pygame.image.load('background.png')
+
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
+    # Events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
